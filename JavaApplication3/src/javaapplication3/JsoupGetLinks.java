a3e4b14db6c5865854f27dd644769c1bedf89330
SIDJ1999 <42091149+SIDJ1999@users.noreply.github.com>
2019-08-08 16:44:39 +0530

    Add files via upload

--- JavaApplication3/src/javaapplication3/JsoupGetLinks.java
+++ JavaApplication3/src/javaapplication3/JsoupGetLinks.java
@@ -4,32 +4,55 @@
  * and open the template in the editor.
  */
 package javaapplication3;
+import java.util.Stack;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
- 
+
 /**
  * This class is used get links from HTML using Jsoup.
  * @author codesjava
  */
 public class JsoupGetLinks {
   public static void main(String args[]){
-    Document document;
+    Document document,document1;
     try {
+        Stack<String> stack=new Stack<>();
+        List<String> visited= new ArrayList<>();
     	//Get Document object after parsing the html from given url.
-	document = Jsoup.connect("https://www.google.com/").get();
+	document = Jsoup.connect("https://www.youtube.com/").get();
+        stack.push("https://www.youtube.com/");
  
 	//Get links from document object. 
 	Elements links = document.select("a[href]");
- 
+        
 	//Iterate links and print link attributes.
 	for (Element link : links) {
+                stack.push(link.attr("href"));
 		System.out.println("Link: " + link.attr("href"));
 		System.out.println("Text: " + link.text());
 		System.out.println("");
 	}
+        for(int i=0;i<10;i++)
+        {
+          String url=stack.pop();
+          visited.add(url);
+          document1 = Jsoup.connect("url").get();
+          Elements links1 = document1.select("a[href]");
+          for (Element link1 : links) {
+                if((!(visited.contains(link1.attr("href")))) && (!(stack.contains(link1.attr("href")))))
+                {
+                     stack.push(link1.attr("href"));
+                     System.out.println("Link: " + link1.attr("href"));
+                     System.out.println("Text: " + link1.text());
+                     System.out.println("");
+                }
+	}
+        }
  
     } catch (IOException e) {
     }		
---
+++ JavaApplication3/src/javaapplication3/NLP.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.IOException;
+import java.util.HashMap;
+import opennlp.tools.tokenize.SimpleTokenizer;
+
+public class NLP { 
+   public static void main(String args[]) throws IOException{ 
+     
+      Document document = Jsoup.connect("https://timesofindia.indiatimes.com/india/un-cautions-india-on-human-rights-violation-even-as-trump-stays-silent-on-kashmir/articleshow/70576921.cms").get();
+      String txt= document.text();
+     // String sentence = "so so so so" + " We provide free tutorials on various technologies"; 
+    
+      //Instantiating SimpleTokenizer class 
+      SimpleTokenizer simpleTokenizer = SimpleTokenizer.INSTANCE;  
+       
+      //Tokenizing the given sentence 
+      String tokens[] = simpleTokenizer.tokenize(txt);  
+       
+      //Printing the tokens 
+  //    for(String token : tokens) {         
+ //        System.out.println(token);  
+  //    }       
+      
+                HashMap < String, Integer > hmap = new HashMap < String, Integer > ();
+          for (String str: tokens) {
+              if (hmap.get(str) == null) 
+              {
+                hmap.put(str, 1);
+              }
+              else {
+              hmap.put(str, hmap.get(str) + 1);
+                 }
+              }
+          System.out.println(hmap.get("Modi"));
+   }  
+}
